26d422aeaa92d65534134333a532943f74dfad9a
Warmelf <warmelf17@gmail.com>
2021-06-13 23:13:28 +0300

    delete comments

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+const complexBlock = document.querySelector('.complex-description-block');
+const body = document.querySelector('body');
+
 function changeItem() {
-    document.querySelector('.complex-description-block').classList.add('appear-complex');
-    document.querySelector('.complex-description-block').classList.remove('hide-complex');
-    document.querySelector('body').style.background = 'linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url(../src/background.jpg)';
+    complexBlock.classList.add('appear-complex');
+    complexBlock.classList.remove('hide-complex');
+    body.style.background = 'linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url(../src/background.jpg)';
 }
 
 function rechangeItem() {
-    document.querySelector('.complex-description-block').classList.remove('appear-complex');
-    document.querySelector('.complex-description-block').classList.add('hide-complex');
-    document.querySelector('body').style.background = '#585148 url(../src/background.jpg) no-repeat';
+    complexBlock.classList.remove('appear-complex');
+    complexBlock.classList.add('hide-complex');
+    body.style.background = '#585148 url(../src/background.jpg) no-repeat';
 }
